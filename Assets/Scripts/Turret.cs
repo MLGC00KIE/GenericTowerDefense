7ae4d5cb7225b2e8456660f0a4ab4210fcce9c56
MLGC00KIE <24893@ma-web.nl>
2018-11-14 13:34:17 +0100

    added grid, basic enemy and game logic

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+    [SerializeField]
+    private float range = 15f;
+
+    public string enemyTag = "enemy";
+
+    private bool isRotateAbleTurret = true;
+
+    private Transform partToRotate = null;
+    [SerializeField]
+    private float turnspeed = 10f;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if (target == null)
+            return;
+
+        if (isRotateAbleTurret)
+        {
+            Vector3 direction = target.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnspeed).eulerAngles;
+
+            partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+
+
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+
+
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
